fa7245c4012cc855bf471fc2bc0526a1b27c625f
Peter Ahn <peterahn8@gmail.com>
2023-09-04 22:49:26 -0400

    Set imports and write placeholder code

--- Program.vb
+++ Program.vb
@@ -1,7 +1,23 @@
-Imports System
+Imports System.Security.Cryptography
+Imports System.IO
+Imports System.Runtime.InteropServices
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        ' Dim key() As Byte = 
+        ' Dim iv() As Byte = 
+
+        Dim inputPath As String = ""
+        Dim encryptedPath As String = ""
+        Dim decrypedPath As String = ""
+
     End Sub
+
+    Function generateRandomKey() As Byte()
+
+    End Function
+
+    Function generateRandomIV() As Byte()
+
+    End Function
 End Module
