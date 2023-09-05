80217c7a08fb44cd6360470b90462d248123642d
Peter Ahn <peterahn8@gmail.com>
2023-09-04 23:35:19 -0400

    Add framework for encrypt/decrypt subs

--- Program.vb
+++ Program.vb
@@ -4,20 +4,34 @@ Imports System.Runtime.InteropServices
 
 Module Program
     Sub Main(args As String())
-        ' Dim key() As Byte = 
-        ' Dim iv() As Byte = 
+        Dim key() As Byte = GenerateRandomKey()
+        Dim iv() As Byte = GenerateRandomIV()
 
         Dim inputPath As String = ""
         Dim encryptedPath As String = ""
         Dim decrypedPath As String = ""
 
+        Try
+            EncryptFile(inputPath, encryptedPath, key, iv)
+
+        Catch ex As Exception
+
+        End Try
     End Sub
 
-    Function generateRandomKey() As Byte()
+    Function GenerateRandomKey() As Byte()
 
     End Function
 
-    Function generateRandomIV() As Byte()
+    Function GenerateRandomIV() As Byte()
 
     End Function
+
+    Sub EncryptFile(inputPath As String, outputPath As String, key As Byte(), iv As Byte())
+
+    End Sub
+
+    Sub DecryptFile(inputPath As String, outputPath As String, key As Byte(), iv As Byte())
+
+    End Sub
 End Module
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Crypta
+
+Brushing up on VB.NET w/ an encryption tool. Planned features: generate random keys and IV's, encrypt a file, decrypt a file
